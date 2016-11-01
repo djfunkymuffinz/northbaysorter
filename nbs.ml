f6a15d7948fceec4c8917a99f0b4ceb3951410a3
Alexander Honer <djfunkymuffins@gmail.com>
2016-11-01 17:50:01 -0400

    woah major restructuring. coming at this in a more ocaml way. also added to better csvs for testing

--- nbs.ml
+++ nbs.ml
@@ -1,26 +1,31 @@
-#use "csv_parse.ml"
-#use "students.ml"
+#load "str.cma"
 
-let csv_to_students file_name =
-   let str_list = lines_to_array (read_file file_name) in
-   let s_list = List.map (fun m_str -> (
-      let name = List.nth m_str 0 in
-      let gender = String.get (List.nth m_str 1) 0 in
-      let chap = ((List.nth m_str 5) = "y") in
-      let f1 = (List.nth m_str 2) in
-      let f2 = (List.nth m_str 3) in
-      let f3 = (List.nth m_str 4) in
-      let std = Student.cons chap name gender in
-      Student.add_friends std (f1::f2::[f3])
-   )) (List.tl str_list) in
-   s_list
+type student =
+   |  None
+   |  Student of string
+   |  Leader of string
 
-let is_student_happy s s_list =
-   List.exists (fun e_s -> (List.mem (Student.name e_s) (Student.friends s))) s_list
+let read_file filename =
+   let lines = ref [] in
+   let chan = open_in filename in
+   try
+      while true; do
+         lines := input_line chan :: !lines
+      done; !lines
+   with End_of_file ->
+      close_in chan;
+      List.rev !lines
 
-let is_valid_group gender s_list =
-   List.fold_left (fun acc fl_s -> (is_student_happy fl_s s_list) && Student.gender fl_s = gender && acc) true s_list
+let lines_to_array lines =
+   let reg_comma = Str.regexp "," in
+   List.map (fun m_line -> List.tl (Str.split reg_comma m_line)) lines
 
-
-let students s_list =
-   List.map (fun m_s -> Student.name m_s) s_list
+let csv_parse file =
+   let lines = lines_to_array (read_file file) in
+   List.map (fun m_l -> 
+      let s = Student (String.trim (List.nth m_l 2)^" "^String.trim (List.nth m_l 1)) in
+      let f1 = Student (String.trim (List.nth m_l 4)^" "^String.trim (List.nth m_l 3)) in
+      let f2 = Student (String.trim (List.nth m_l 6)^" "^String.trim (List.nth m_l 5)) in
+      let f3 = Student (String.trim (List.nth m_l 8)^" "^String.trim (List.nth m_l 7)) in
+      (s,[f1;f2;f3])
+   ) lines
